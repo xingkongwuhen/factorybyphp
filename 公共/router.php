6bcd4e9e81197db1170b7ab8452a23043c5dd820
xingkongwuhen <49484827+xingkongwuhen@users.noreply.github.com>
2022-07-16 09:38:28 +0800

    Add files via upload

--- 公共/router.php
+++ 公共/router.php
@@ -2,16 +2,19 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+// $Id$
 
-// [ 应用入口文件 ]
-
-// 定义应用目录
-define('APP_PATH', __DIR__ . '/../application/');
-// 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
+    return false;
+} else {
+    if (!isset($_SERVER['PATH_INFO'])) {
+        $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
+    }
+    require __DIR__ . "/index.php";
+}
---
+++ 公共/webconfig.php
@@ -0,0 +1,8 @@
+<?php 
+$webname = "Shop商城";
+$webtel = "15806850547";
+$webdescription = "这是一个正在搭建中的网站。具体信息请联系官网！！！";
+$weblogo = "/public/uploads/img/20181113\956eeaa551686ef1bec0d124469e5dc0.jpg";
+$webemail = "2639547104@qq.com";
+$webkeywords = "自定义关键词";
+ ?>
